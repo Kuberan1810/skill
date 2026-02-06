bd7f4a5689f74b1fb7ab36d6bee139aeb63c379f
Kuberan1810 <144237380+Kuberan1810@users.noreply.github.com>
2026-02-07 04:05:29 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/payment", require("./routes/payment"));
+
+app.listen(5000, () => {
+    console.log("Backend running on port 5000");
+});
