696ab6de787bc5d05e97dcbab07db9c099fb728e
Kuberan1810 <144237380+Kuberan1810@users.noreply.github.com>
2026-02-07 04:50:09 +0530

    add .

--- server.js
+++ server.js
@@ -7,9 +7,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/payment", require("/routes/payment"));
+app.use("/api/payment", require("./routes/payment"));
 
 app.listen(5000, () => {
     console.log("Backend running on port 5000");
 });
+
 
