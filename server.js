e5e643c8f929edb4d55d2d754e1b3f0ff817c1a0
Kuberan1810 <144237380+Kuberan1810@users.noreply.github.com>
2026-02-07 04:46:17 +0530

    Format server.js for consistency  in remove .

--- server.js
+++ server.js
@@ -7,8 +7,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/payment", require("./routes/payment"));
+app.use("/api/payment", require("/routes/payment"));
 
 app.listen(5000, () => {
     console.log("Backend running on port 5000");
 });
+
